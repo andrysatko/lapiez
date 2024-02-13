e012908b3749dbebd661f62e678f5d559ecb3b0f
andrysatko <andrysatko@gmail.com>
2024-02-13 02:58:58 +0200

    pirma module, fixed ci

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './product/product.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    AuthModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
